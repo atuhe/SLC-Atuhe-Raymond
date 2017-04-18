8af37d094498ad8e074092557f171f9a99c81389
atuhe <ratuhe60@gmail.com>
2017-04-18 12:59:11 +0300

    modified the prime number generator to cater for even numbers

--- prime_Number.py
+++ prime_Number.py
@@ -1,2 +1,13 @@
-def generate_prime_numbers():
-	return [3, 2]
+def prime_number_generator(n):
+    j = 2
+    primes = []
+    while j <= n:
+        k = 2
+        while not (k == j) and not (j % k == 0):
+            k = k + 1
+            if k == j:
+                primes.append(j)
+        j = j + 1
+    return primes
+
+print (prime_number_generator(15))
--- prime_tests.py
+++ prime_tests.py
@@ -1,32 +1,32 @@
 import unittest
-import prime_Number as prime
+import prime_Number as prime_function
 
 
 class PrimeTest_case(unittest.TestCase):
 	def setUp(self):
-		self.result = prime.generate_prime_numbers()
+		self.value = prime_function.prime_number_generator(10)
 
 	def testing_lists_return_type(self):
-		self.assertIsInstance(self.result, list)
+		self.assertIsInstance(self.value, list)
 
 	def testing_typeInteger(self):
-		for x in self.result:
+		for x in self.value:
 			self.assertIsInstance(x, int)
 
 	def test_negation(self):
-		for x in self.result:
+		for x in self.value:
 			self.assertGreater(x, 0)
 
 	def testing_single_entry(self):
-		for x in self.result:
+		for x in self.value:
 			self.assertFalse(x == 1)
 
 	def test_prime_even_number(self):
-		for x in self.result:
+		for x in self.value:
 			self.assertFalse(x > 2 and x % 2 == 0)
 
 	def test_divisible_by_three_number(self):
-		for x in self.result:
+		for x in self.value:
 			self.assertFalse(x > 3 and x % 3 == 0)
 
 
